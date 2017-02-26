9cb1c956b5b8f079028e0189bc477d37c5ddf683
jmontene <josmontenegro2012@gmail.com>
2017-02-26 17:26:36 -0500

    Current minicode: Adding a 2d outline fire effect to sprites

---
+++ Assets/Scripts/FireParticle.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FireParticle : MonoBehaviour {
+
+	public Vector2 minVelocity = new Vector2(-0.1f,0.2f);
+	public Vector2 maxVelocity = new Vector2(0.1f,0.4f);
+
+	public float lifeSpan = 2f;
+
+	float actualLifeSpan;
+	float timeAlive = 0f;
+	Vector2 velocity;
+
+	// Use this for initialization
+	void OnEnable() {
+		velocity = new Vector2(Random.Range(minVelocity.x,maxVelocity.x), Random.Range(minVelocity.y,maxVelocity.y));
+		actualLifeSpan = lifeSpan * Random.Range(0.9f,1.1f);
+		timeAlive = 0f;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		timeAlive += Time.deltaTime;
+		if(timeAlive >= actualLifeSpan){
+			SimplePool.Despawn(gameObject);
+			return;
+		}
+		transform.Translate(velocity*Time.deltaTime);
+	}
+}
---
+++ Assets/Scripts/FireParticleEffect.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FireParticleEffect : MonoBehaviour {
+
+	public GameObject particlePrefab;
+	public float ratePerSecond = 500f;
+
+	protected float timeSinceLastSpawn = 0f;
+	protected PolygonCollider2D pCollider;
+
+	// Use this for initialization
+	void Start () {
+		pCollider = GetComponent<PolygonCollider2D>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		timeSinceLastSpawn += Time.deltaTime;
+		float correctTime = 1f/ratePerSecond;
+
+		while(timeSinceLastSpawn > correctTime){
+			SpawnFireAlongOutline();
+			timeSinceLastSpawn -= correctTime;
+		}
+	}
+
+	void SpawnFireAlongOutline(){
+		int pathIndex = Random.Range(0,pCollider.pathCount);
+		Vector2[] points = pCollider.GetPath(pathIndex);
+		int pointIndex = Random.Range(0,points.Length);
+		Vector2 spawnPointA = points[pointIndex];
+		Vector2 spawnPointB = points[(pointIndex+1) % points.Length];
+
+		Vector2 spawnPoint = Vector2.Lerp(spawnPointA,spawnPointB, Random.Range(0f,1f));
+		SpawnFireAtPosition((spawnPoint + (Vector2)transform.position)*(Vector2) transform.localScale);
+	}
+
+	void SpawnFireAtPosition(Vector2 position){
+		SimplePool.Spawn(particlePrefab,position, Quaternion.identity);
+	}
+}
